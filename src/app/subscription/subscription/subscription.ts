7685e967cf0f7ba94cde6908a34050c9bb986d35
b3r1u <roberioalbuquerque.neto@gmail.com>
2026-05-15 20:23:20 -0300

    chore: remove arquivos para recriar projeto com Angular 17

--- src/app/subscription/subscription/subscription.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-subscription',
-  standalone: false,
-  templateUrl: './subscription.html',
-  styleUrl: './subscription.scss',
-})
-export class Subscription {}

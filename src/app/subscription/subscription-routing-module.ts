27627f77e37544ad2b19cb4b4c9a515fdac06340
b3r1u <roberioalbuquerque.neto@gmail.com>
2026-05-15 19:46:14 -0300

    feat: configurando rotas do módulo de assinaturas

--- src/app/subscription/subscription-routing-module.ts
+++ src/app/subscription/subscription-routing-module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Subscription } from './subscription/subscription';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', component: Subscription }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
